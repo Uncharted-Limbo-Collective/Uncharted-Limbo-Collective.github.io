cf3dbf6cfb6db35fb9beecb8571ab431b6f7c1fd
GeorgeAdamon <30707361+GeorgeAdamon@users.noreply.github.com>
2021-05-22 18:22:22 +0100

    Fixed auto-stop of videos once and for all

---
+++ js/stop-videos.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict"; // Start of use strict
+
+// any modals with embedded <iframe> we can assume need reset
+// <https://stackoverflow.com/a/52315492>
+//
+    $('body').on('hide.bs.modal', '.modal', function () {
+        const $modal = $(this);
+        // return early if there were no embedded YouTube videos
+        if ($modal.find('iframe').length === 0) return;
+        const html = $modal.html();
+        $modal.html(html);
+    });
+})(jQuery); // End of use strict
