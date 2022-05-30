304460cc2ba3818cdd5be7c2da7eb19ea12f4b1a
EleanaPol <45208539+EleanaPol@users.noreply.github.com>
2022-05-30 20:27:38 +0100

    co mmit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -41,10 +41,10 @@
       });
 
     // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
+    /*$('body').scrollspy({
       target: '#mainNav',
       offset: 80
-    });
+    });*/
 
     // Collapse Navbar
     var navbarCollapse = function() {
